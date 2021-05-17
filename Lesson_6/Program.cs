b9cc4178a819f95900cdb9032a74bb936cedc4f7
yurec0098 <yurec0098@rambler.ru>
2021-05-17 06:27:06 +0300

    Добавьте файлы проекта.

---
+++ Lesson_6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Lesson_6
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
